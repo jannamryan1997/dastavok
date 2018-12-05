622d9e441a765362f2a7cc33beac779139d90fe0
rado <rado.poghosyan@annaniks.com>
2018-12-05 14:21:57 +0400

    changed choose bar

--- src/app/com/annaniks/dastavok/views/main/home/home.view.ts
+++ src/app/com/annaniks/dastavok/views/main/home/home.view.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core"
 import { MenuItemsService } from "../../../services";
 import { SignUpService } from "../../../services/signUp.service";
+import { Router, NavigationEnd } from "@angular/router";
+
 
 @Component({
     selector: "app-home",
@@ -9,9 +11,21 @@ import { SignUpService } from "../../../services/signUp.service";
 })
 
 export class HomeView implements OnInit {
+    public chooseBarVisiblity:boolean = true;
 
-    constructor(public menuItemsService: MenuItemsService,public signUpService:SignUpService) {}
+    constructor(public menuItemsService: MenuItemsService,public signUpService:SignUpService,private _router:Router) {}
+    
+    ngOnInit() {
+        this._checkWindowSize();
+    }
 
-
-    ngOnInit() {}
+    private _checkWindowSize(){
+        if(this._router.url!='/home/information' && window.innerWidth<=900){
+            this._router.events.forEach((event)=>{
+                if(event instanceof NavigationEnd && event.url!='/home/information'){
+                    this.chooseBarVisiblity=false;
+                }
+            })
+        }
+    }
 }
