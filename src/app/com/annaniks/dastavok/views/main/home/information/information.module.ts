8ac12cf9750fbe3c028df7babdf718b4f6d16fa9
jannamryan1997 <jannamryan8@gmail.com>
2019-10-23 14:54:23 +0400

    crated DV_6 task

--- src/app/com/annaniks/dastavok/views/main/home/information/information.module.ts
+++ src/app/com/annaniks/dastavok/views/main/home/information/information.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from "@angular/core"
 import { InformationView } from "./information.view";
 import { InformationRoutingModule } from "./information.routing.module";
-import { SliderComponent, CompanyListComponent, CompanyListItemComponent} from "../../../../components";
-import { NgxCarouselModule } from 'ngx-carousel';
-import { CommonModule } from "@angular/common"
-import { SharedModule } from "../../../../shared/shared.module";
-import { InformationService } from "./information.service";
-import {ProgressBarModule} from 'primeng/progressbar';
+// import { SliderComponent, CompanyListComponent, CompanyListItemComponent} from "../../../../components";
+// import { NgxCarouselModule } from 'ngx-carousel';
+// import { CommonModule } from "@angular/common"
+// import { SharedModule } from "../../../../shared/shared.module";
+// import { InformationService } from "./information.service";
+// import {ProgressBarModule} from 'primeng/progressbar';
 @NgModule({
-    declarations: [InformationView, SliderComponent, CompanyListComponent, CompanyListItemComponent],
-    imports: [InformationRoutingModule, NgxCarouselModule, CommonModule, SharedModule,ProgressBarModule],
-    providers:[InformationService],
+    declarations: [InformationView],
+    imports: [InformationRoutingModule],
+    providers:[],
     exports: [],
 })
 
