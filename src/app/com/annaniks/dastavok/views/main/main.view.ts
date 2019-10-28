bc6b8893e63bc5af8f47b18969e5ec46fc77df75
jannamryan1997 <jannamryan8@gmail.com>
2019-10-28 14:33:47 +0400

    created DV_12 task

--- src/app/com/annaniks/dastavok/views/main/main.view.ts
+++ src/app/com/annaniks/dastavok/views/main/main.view.ts
@@ -1,20 +1,15 @@
-import {Component, OnInit} from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { MatDialog } from "@angular/material";
+import { UserUpdateModal } from "../../modals";
 
 @Component({
-    selector:"app-main",
-    templateUrl:"main.view.html",
-    styleUrls:["main.view.scss"]
+    selector: "app-main",
+    templateUrl: "main.view.html",
+    styleUrls: ["main.view.scss"]
 })
 
-export class MainComponent implements OnInit{
+export class MainComponent implements OnInit {
 
-    constructor(){
-      
-    }
+    ngOnInit() {}
 
-    ngOnInit(){
-
-    }
-
-    
 }
