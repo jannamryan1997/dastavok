e008af1728a670fcd2113cc2fd7e756964175cc3
Rado <rado.poghosyan@annaniks.com>
2019-11-07 11:15:59 +0400

    completed B_42

--- src/app/com/annaniks/dastavok/modals/search-goods/search-goods.modal.ts
+++ src/app/com/annaniks/dastavok/modals/search-goods/search-goods.modal.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { MatDialogRef } from "@angular/material";
+import { FormControl } from "@angular/forms";
 
 @Component({
     selector: "app-search-goods",
@@ -10,13 +11,14 @@ import { MatDialogRef } from "@angular/material";
 
 export class SearchGoodsModals implements OnInit {
 
-    public search: string;
+    public searchControl: FormControl = new FormControl('');
+
     ngOnInit() { }
 
     constructor(private _router: Router, private _dialogRef: MatDialogRef<SearchGoodsModals>) { }
 
     public onClickSearch(): void {
-        this._router.navigate(["home/search"], { queryParams: { search: this.search } })
+        this._router.navigate(["/search"], { queryParams: { search: this.searchControl.value } })
         this._dialogRef.close();
     }
 
